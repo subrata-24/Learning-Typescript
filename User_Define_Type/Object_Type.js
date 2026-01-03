84830e6e48a9d54bd74d5ec85ab5ac249dc4314f
Subrata Biswas <subrata17@cse.pstu.ac.bd>
2026-01-03 20:00:13 +0600

    Learn object type

---
+++ User_Define_Type/Object_Type.js
@@ -0,0 +1,30 @@
+/*
+    TypeScript allows us to define the exact structure (shape) of an object.We can make certain properties optional using the '?' symbol, which is extremely useful for data that might not always be present.
+*/
+var user1;
+user1 = { name: "Subrata Biswas", id: 1902014 };
+console.log(user1);
+// Here, 'id' is optional, so we can initialize the object with just a name.
+var user2 = {
+    name: "Sabuj Biswas"
+};
+console.log(user2);
+/*
+    Defining an Array of Specific Objects:
+    INSTEAD OF: let users: object[] = [];
+    WE USE: A specific shape so we can access properties safely later.
+*/
+var users = [];
+users.push(user1);
+users.push(user2);
+console.log(users);
+// Correct Way to Loop in array -> 'for...of'
+for (var _i = 0, users_1 = users; _i < users_1.length; _i++) {
+    var user = users_1[_i];
+    console.log(user.name);
+}
+/*
+ - Avoid using the generic 'object' type; it prevents you from accessing specific properties.
+ - Use the '?' modifier to handle data that is missing or undefined.
+ - Use 'for...of' for iterating over array values and 'for...in' for object keys.
+*/ 
