2842d8c2a9f6c0b33dac67611e4d88da2ac2613a
Subrata Biswas <subrata17@cse.pstu.ac.bd>
2026-01-03 19:21:08 +0600

    Learn any type

---
+++ User_Define_Type/Any_Type.js
@@ -0,0 +1,9 @@
+var user; //lost the type safety
+user = 24;
+console.log(user);
+user = "Subrata";
+console.log(user);
+user = false;
+console.log(user);
+user = [24, "Subrata"];
+console.log(user);
---
+++ User_Define_Type/Any_Type.ts
@@ -0,0 +1,10 @@
+let user: any;//lost the type safety
+
+user = 24;
+console.log(user)
+user = "Subrata"
+console.log(user)
+user = false;
+console.log(user)
+user = [24, "Subrata"]
+console.log(user)
