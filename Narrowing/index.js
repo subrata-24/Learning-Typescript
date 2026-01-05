38b56797894da99223474953282e93864de9a5ff
Subrata Biswas <subrata17@cse.pstu.ac.bd>
2026-01-06 01:26:17 +0600

    Learn basic about Narrowing

---
+++ Narrowing/index.js
@@ -0,0 +1,18 @@
+function printTodos(todos) {
+    if (todos) { // Truthy / falsy narrowing
+        if (typeof todos === "object") { // typeof narrowing
+            todos.map(function (todo) { return console.log(todo); });
+        }
+        else {
+            console.log(todos);
+        }
+    }
+    else {
+        console.log("There is no do");
+    }
+}
+var todos = ["todo1", "todo2"];
+printTodos(todos);
+printTodos("Todoss");
+printTodos(null);
+// printTodos(); pass undefined
---
+++ Narrowing/index.ts
@@ -0,0 +1,17 @@
+function printTodos(todos: string[] | string | null) {
+    if (todos) { // Truthy / falsy narrowing
+        if (typeof todos === "object") { // typeof narrowing
+            todos.map((todo)=>console.log(todo))
+        } else {
+            console.log(todos);
+        }
+    }else {
+            console.log("There is no do")
+        }
+}
+
+let todos = ["todo1", "todo2"];
+printTodos(todos);
+printTodos("Todoss");
+printTodos(null);
+// printTodos(); pass undefined
